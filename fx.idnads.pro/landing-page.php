dce28f899e40105b34dd352f48a959109c4e0fd3
icefutures <114502305+icefutures@users.noreply.github.com>
2026-01-29 00:29:31 +0000

    Refactor code structure for improved readability and maintainability

--- fx.idnads.pro/landing-page.php
+++ fx.idnads.pro/landing-page.php
@@ -84,15 +84,17 @@
             </div>
             <div class="slideshow-container">
                 <div class="mySlides">
-                    <img src="https://go.idnads.pro/img/modalinv2.png" alt="Testimonial 1" onclick="openModal(this.src)">
+                    <img src="https://fx.idnads.pro/images/testimonnial/testi1.jpg" alt="Testimonial 1" onclick="openModal(this.src)">
                 </div>
                 <div class="mySlides">
-                    <img src="https://go.idnads.pro/img/modalinv2.png" alt="Testimonial 2" onclick="openModal(this.src)">
+                    <img src="https://fx.idnads.pro/images/testimonnial/testi2.jpg" alt="Testimonial 2" onclick="openModal(this.src)">
                 </div>
                 <div class="mySlides">
-                    <img src="https://go.idnads.pro/img/modalinv2.png" alt="Testimonial 3" onclick="openModal(this.src)">
+                    <img src="https://fx.idnads.pro/images/testimonnial/testi3.jpg" alt="Testimonial 3" onclick="openModal(this.src)">
                 </div>
-
+                <div class="mySlides">
+                    <img src="https://fx.idnads.pro/images/testimonnial/testi4.jpg" alt="Testimonial 4" onclick="openModal(this.src)">
+                </div>
                 <div class="dots-container">
                     <span class="dot" onclick="currentSlide(1)"></span>
                     <span class="dot" onclick="currentSlide(2)"></span>
